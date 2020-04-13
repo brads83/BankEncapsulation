2c3236189b04b318020356cfc582a6ed13d1d365
brads83 <bradsorrells73@gmail.com>
2020-04-13 13:23:37 -0500

    Add project files.

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            var amount = 300;
+            account.Deposit(amount);
+            account.GetBalance();
+            Console.WriteLine($"Balance = {account.GetBalance()}");
+        }
+    }
+}
